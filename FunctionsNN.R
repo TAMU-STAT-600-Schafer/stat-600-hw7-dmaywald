d9becb923b1846168fbeded4e2e389fdd24ecd6e
Devon Maywald <dmaywald@tamu.edu>
2024-11-18 20:20:08 -0600

    add functions and tests

--- FunctionsNN.R
+++ FunctionsNN.R
@@ -30,9 +30,16 @@ initialize_bw <- function(p, hidden_p, K, scale = 1e-3, seed = 12345){
 # K - number of classes
 loss_grad_scores <- function(y, scores, K){
   
+  n = length(y)
   # [ToDo] Calculate loss when lambda = 0
-  expScores = exp(Scores)
+  expScores = exp(scores)
   probs = expScores / rowSums(expScores)
+  
+  # Sometimes values in scores_true are too large and result in NaN in true_probs
+  # If this occurs, find NaN values and replace with 1
+  if (anyNA(probs)) {
+    probs[is.na(probs)] <- 1
+  }
   
   loss = 0
   
@@ -40,15 +47,84 @@ loss_grad_scores <- function(y, scores, K){
     loss = loss + sum(log(probs[y == (i-1), i]))
   }
   
-  loss = (-1/length(y)) * loss
-  
+  loss = -1 * (loss/n)
+               
   # [ToDo] Calculate misclassification error rate (%)
   # when predicting class labels using scores versus true y
-  error = 100 * (1 - mean(max.col(probs) == (y+1)))
+  # use rowwise maximum of SCORES rather than probs. 
+  # scores are less likely to have "tie breakers" needed
+  # not a perfect solution, sometimes tie breakers are still needed
+  # "tie breakers" are done by selecting the first column containing the minimal value
+  error = 100 * (1 - mean(max.col(scores) == (y+1)))
   
   # [ToDo] Calculate gradient of loss with respect to scores (output)
   # when lambda = 0
-  # grad = ...
+  one_hot_mat = matrix(1 -  model.matrix(~ -1+factor(y, levels = 0:(K-1))), nrow = n)
+  
+  
+  grad = probs * one_hot_mat
+  
+  
+  # Return loss, gradient and misclassification error on training (in %)
+  return(list(loss = loss, grad = grad, error = error))
+}
+
+
+loss_grad_scores_2 <- function(y, scores, K){
+  
+  n = length(y)
+  # [ToDo] Calculate loss when lambda = 0
+  expScores = exp(scores)
+  probs = expScores / rowSums(expScores)
+  
+  # Sometimes values in scores_true are too large and result in NaN in true_probs
+  # If this occurs, find NaN values and replace with 1
+  if (anyNA(probs)) {
+    probs[is.na(probs)] <- 1
+  }
+  
+  loss = 0
+  
+  for (i in 1:K) {
+    loss = loss + sum(log(probs[y == (i-1), i]))
+  }
+  
+  loss = -1 * (loss/n)
+  
+  # [ToDo] Calculate misclassification error rate (%)
+  # when predicting class labels using scores versus true y
+  # use rowwise maximum of SCORES rather than probs. 
+  # scores are less likely to have "tie breakers" needed
+  # not a perfect solution, sometimes "tie breakers" are still needed
+  # "tie breakers" are done by selecting the first column containing the minimal value
+  # error = 100 * (1 - mean(max.col(scores) == (y+1)))
+  
+  error = 0
+  
+  for (i in 1:n) {
+    # if the i'th row of scores does not have the i'th classification of y, then increase error count
+    if(!any(which(scores[i, ] == max(scores[i, ])) == (y+1))){
+      error = error + 1
+    }
+  }
+  
+  error = 100*(error/n)
+  
+  # [ToDo] Calculate gradient of loss with respect to scores (output)
+  # when lambda = 0
+  
+  # This for loop operates similarly to "table(y)/n",
+  # but "table(y)/n" will not make a save counts of classifications that does
+  # not appear
+  # Ex: if y = c(0,0,2,3,4), then table(y)/n gives ".4 .2 .2 .2" instead of ".4 0 .2 .2 .2"
+  mean_counts = rep(0, K)
+  
+  for (i in 0:(K-1)) {
+    mean_counts[i] = mean(y == i)
+  }
+  
+  grad = colMeans(probs) - mean_counts
+  
   
   # Return loss, gradient and misclassification error on training (in %)
   return(list(loss = loss, grad = grad, error = error))
@@ -66,19 +142,31 @@ loss_grad_scores <- function(y, scores, K){
 one_pass <- function(X, y, K, W1, b1, W2, b2, lambda){
 
   # [To Do] Forward pass
-  # From input to hidden 
+  # From input to hidden and add bias
+  H <- X %*% W1 + matrix(b1, nrow = length(y), ncol = length(b1), byrow = T)
   
   # ReLU
+  H = (abs(H) + H)/2
   
   # From hidden to output scores
- 
+  # print(dim(H))
+  # print(dim(W2))
+  # print(dim(matrix(b2, nrow = length(y), ncol = length(b2), byrow = T)))
+  scores = H %*% W2 + matrix(b2, nrow = nrow(H), ncol = ncol(W2), byrow = T)
   
   # [ToDo] Backward pass
   # Get loss, error, gradient at current scores using loss_grad_scores function
+  out = loss_grad_scores(y, scores, K)
 
   # Get gradient for 2nd layer W2, b2 (use lambda as needed)
+  dW2 = crossprod(H, out$grad) + lambda*W2
+  db2 = rowSums(out$grad)
   
   # Get gradient for hidden, and 1st layer W1, b1 (use lambda as needed)
+  dH  = tcrossprod(out$grad, W2)
+  dA1 = (abs(dH) + dH)/2
+  dW1 = crossprod(X, dA1) + lambda*W1
+  db1 = colSums(dA1)
   
   # Return output (loss and error from forward pass,
   # list of gradients from backward pass)
@@ -96,8 +184,18 @@ one_pass <- function(X, y, K, W1, b1, W2, b2, lambda){
 evaluate_error <- function(Xval, yval, W1, b1, W2, b2){
   # [ToDo] Forward pass to get scores on validation data
   
+  # First layer and bias
+  H = Xval %*% W1 + matrix(b1, nrow = length(yval), ncol = length(b1), byrow = T)
+  
+  # RELU
+  H = (abs(H) + H) / 2
+  
+  # 2nd layer and bias
+  scores = H %*% W2 + matrix(b2, nrow = nrow(H), ncol = ncol(W2), byrow = T)
+  
   # [ToDo] Evaluate error rate (in %) when 
   # comparing scores-based predictions with true yval
+  error = 100 * (1 - mean(max.col(scores) == (yval+1)))
   
   return(error)
 }
@@ -122,9 +220,24 @@ NN_train <- function(X, y, Xval, yval, lambda = 0.01,
   # Get sample size and total number of batches
   n = length(y)
   nBatch = floor(n/mbatch)
+  K = max(c(y, yval)) + 1
 
   # [ToDo] Initialize b1, b2, W1, W2 using initialize_bw with seed as seed,
   # and determine any necessary inputs from supplied ones
+  p = ncol(X)
+  
+  init_params = initialize_bw(
+    p = p,
+    hidden_p = hidden_p,
+    K = K,
+    scale = scale,
+    seed = seed
+  )
+  
+  W1 = init_params$W1
+  W2 = init_params$W2
+  b1 = init_params$b1
+  b2 = init_params$b2
   
   # Initialize storage for error to monitor convergence
   error = rep(NA, nEpoch)
@@ -136,14 +249,32 @@ NN_train <- function(X, y, Xval, yval, lambda = 0.01,
   for (i in 1:nEpoch){
     # Allocate bathes
     batchids = sample(rep(1:nBatch, length.out = n), size = n)
+    # Accumulate error over batches, and compute average
+    cur_err = 0
     # [ToDo] For each batch
-    #  - do one_pass to determine current error and gradients
-    #  - perform SGD step to update the weights and intercepts
-    
+    for (j in 1:nBatch) {
+      #  - do one_pass to determine current error and gradients
+      pass = one_pass(X = X[batchids == j, , drop = FALSE], y = y[batchids == j], K = K,
+                      W1 =  W1, b1 = b1, W2 = W2, b2 = b2, lambda = lambda)
+      
+      # print(pass$error)
+      cur_err = cur_err + pass$error
+      
+      #  - perform SGD step to update the weights and intercepts
+
+      W1 <- W1 - rate * pass$grads$dW1
+      b1 <- b1 - rate * pass$grads$db1
+      W2 <- W2 - rate * pass$grads$dW2
+      b2 <- b2 - rate * pass$grads$db2
+      
+    }
     # [ToDo] In the end of epoch, evaluate
     # - average training error across batches
+    error[i] = cur_err/nBatch
     # - validation error using evaluate_error function
+    error_val[i] = evaluate_error(Xval, yval, W1, b1, W2, b2)
   }
+  
   # Return end result
   return(list(error = error, error_val = error_val, params =  list(W1 = W1, b1 = b1, W2 = W2, b2 = b2)))
 }
---
+++ Tests.R
@@ -0,0 +1,154 @@
+library(testthat)
+
+################## Test Function initialize_bw #################################
+for (i in 1:5) {
+  p = sample(1:15, size = 1)
+  hidden_p = sample(6:20, size = 1)
+  K = sample(1:10, size = 1)
+  
+  init_params = initialize_bw(p, hidden_p, K)
+  
+  test_that("Check dimensions of initialized parameters",{
+    expect_true(length(init_params$b1) == hidden_p) # first bias is a vector of dimension 'hidden p'
+    expect_true(length(init_params$b2) == K) # second bias is a vector of dimension 'K'
+    expect_true(all(dim(init_params$W1) == c(p, hidden_p))) # first weight matrix has dimensions 'p by hidden p'
+    expect_true(all(dim(init_params$W2) == c(hidden_p, K))) # second weight matrix has dimensions 'hidden p by K'
+  })
+}
+
+
+
+
+
+############# Test Function loss_grad_scores ###################################
+
+p = 7
+hidden_p = 15
+K = 10
+n = 500
+sd_val = 4
+drop_out_rate = .25
+
+# Calculate the probabilities of each class by assigning "true" parameters to pass 
+# through a fully connected NN. 
+# This allows for the target function to be explicitly in the model space 
+X <- matrix(rnorm(n*p), nrow = n)
+
+# For W1, randomly generate values and drop some of them
+W1_vals = rnorm(p*hidden_p, sd = sd_val)
+W1_vals[sample(1:(hidden_p*p), size = floor(drop_out_rate*hidden_p*p), replace = F)] <- 0 # drop some of the values to 0
+W1_true = matrix(W1_vals, nrow = p, ncol = hidden_p)
+
+# for b1, randomly generate values and drop some of them 
+b1_true = rnorm(hidden_p, sd = sd_val)
+b1_true[sample(hidden_p, size = floor(drop_out_rate*hidden_p), replace = F)] <- 0 # drop some of the values to 0
+
+# For W2, randomly generate values and drop some of them
+W2_vals = rnorm(K*hidden_p, sd = sd_val)
+W2_vals[sample(1:(hidden_p*K), size = floor(drop_out_rate*hidden_p*K), replace = F)] <- 0 # drop some of the values to 0
+W2_true = matrix(W2_vals, nrow = hidden_p, ncol = K)
+
+
+# for b2, randomly generate values and drop some of them 
+b2_true = rnorm(K, sd = sd_val)
+b2_true[sample(K, size = floor(drop_out_rate*K), replace = F)] <- 0 # drop some of the values to 0
+
+# Pass X through fully connected network with true parameters
+# First layer and bias
+scores_true = X %*% W1_true + matrix(b1_true, nrow = n, ncol = hidden_p, byrow = T)
+# RELU
+scores_true = (abs(scores_true) + scores_true) / 2
+# 2nd layer and bias
+scores_true = scores_true %*% W2_true + matrix(b2_true, nrow = n, ncol = K, byrow = T)
+# Calculate probabilities based on these scores
+true_probs = exp(scores_true)
+true_probs = true_probs/rowSums(true_probs)
+
+# Sometimes values in scores_true are too large and result in NaN in true_probs
+# If this occurs, find NaN values and replace with 1
+if (anyNA(true_probs)) {
+  true_probs[is.na(true_probs)] <- 1
+}
+
+# assign classifications of y based on true probs
+y <- vector(mode = 'double', length = n)
+for (i in 1:n) {
+  y[i] = sample.int(K, size = 1, prob = true_probs[i, ]) - 1
+}
+
+loss_grad_scores(y, scores = scores_true, K = K)
+
+
+######################## Test Function one pass ################################
+
+pass = one_pass(X = X, y = y, K =  K, W1 = W1_true, b1 = b1_true, W2 = W2_true, b2 = b2_true, lambda = .01)
+pass$grads$dW2
+
+
+
+
+######################### Test Function NN_train ###############################
+p = 5
+hidden_p = 10
+K = 3
+n = 2200
+nval = 200
+sd_val = 5
+drop_out_rate = .25
+
+# Calculate the probabilities of each class by assigning "true" parameters to pass 
+# through a fully connected NN. 
+# This allows for the target function to be explicitly in the model space 
+Xdata <- matrix(rnorm(n*p), nrow = n)
+
+# For W1, randomly generate values and drop some of them
+W1_vals = rnorm(p*hidden_p, sd = sd_val)
+W1_vals[sample(1:(hidden_p*p), size = floor(drop_out_rate*hidden_p*p), replace = F)] <- 0 # drop some of the values to 0
+W1_true = matrix(W1_vals, nrow = p, ncol = hidden_p)
+
+# for b1, randomly generate values and drop some of them 
+b1_true = rnorm(hidden_p, sd = sd_val)
+b1_true[sample(hidden_p, size = floor(drop_out_rate*hidden_p), replace = F)] <- 0 # drop some of the values to 0
+
+# For W2, randomly generate values and drop some of them
+W2_vals = rnorm(K*hidden_p, sd = sd_val)
+W2_vals[sample(1:(hidden_p*K), size = floor(drop_out_rate*hidden_p*K), replace = F)] <- 0 # drop some of the values to 0
+W2_true = matrix(W2_vals, nrow = hidden_p, ncol = K)
+
+
+# for b2, randomly generate values and drop some of them 
+b2_true = rnorm(K, sd = sd_val)
+b2_true[sample(K, size = floor(drop_out_rate*K), replace = F)] <- 0 # drop some of the values to 0
+
+# Pass X through fully connected network with true parameters
+# First layer and bias
+scores_true = Xdata %*% W1_true + matrix(b1_true, nrow = n, ncol = hidden_p, byrow = T)
+# RELU
+scores_true = (abs(scores_true) + scores_true) / 2
+# 2nd layer and bias
+scores_true = scores_true %*% W2_true + matrix(b2_true, nrow = n, ncol = K, byrow = T)
+# Calculate probabilities based on these scores
+true_probs = exp(scores_true)
+true_probs = true_probs/rowSums(true_probs)
+
+# Sometimes values in scores_true are too large and result in NaN in true_probs
+# If this occurs, find NaN values and replace with 1
+if (anyNA(true_probs)) {
+  true_probs[is.na(true_probs)] <- 1
+}
+
+# assign classifications of y based on true probs
+ydata <- vector(mode = 'double', length = n)
+for (i in 1:n) {
+  ydata[i] = sample.int(K, size = 1, prob = true_probs[i, ]) - 1
+}
+
+Xval = Xdata[(n-nval+1):n, , drop = FALSE]
+yval = ydata[(n-nval+1):n]
+Xtrain = Xdata[1:(n-nval), , drop = FALSE]
+ytrain = ydata[1:(n-nval)]
+
+out = NN_train(X = Xtrain, y = ytrain, Xval = Xval, yval = yval, lambda = .01, rate = .01, 
+         mbatch = 20, nEpoch = 100, hidden_p = hidden_p, scale = 1e-3, seed = 12345)
+
+
